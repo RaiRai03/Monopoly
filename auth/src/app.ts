7190f89018aac3808af4e816426e9a43cb4fe5a9
Sarai Sanford <railove017@gmail.com>
2024-08-12 22:15:06 -0400

    Tw Session 7-3

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response} from "express";
 import mongoose from "mongoose";
 import User from "./models/User";
+import jwt from "jsonwebtoken";
+import { expressjwt, Request as JWT_SECRET } from "express-jwt"
 
 const app: Application = express();
 const PORT = 6001;
-
+const JWT_SECRET = "planetKrypton";
 app.use(express.urlencoded({ extended: false }), express.json());
 app. get("/", ( req, res,) => {
   res.send("hello world with typescript to my lovely people, expect the build keeps breaking!");
@@ -14,9 +16,7 @@ app.post("/register", async (req, res) => {
   try {
     const { username, password } = req.body;
     const user = await new User({ username, password });
-    console.log(user);
-    const afterSave = await user.save()
-    console.log(afterSave);
+    await user.save();
     res.sendStatus(200);
   } catch (e) {
     res.sendStatus(500);
@@ -29,12 +29,20 @@ app.post("/login", async(req,res)=>{
   if(!user) res.sendStatus(404)
   const handler = (isValidPassword: boolean) => {
     if(isValidPassword) {
-     res.json({user: user!.username});
+     res.json({
+        jwt: jwt.sign({ user: user!.username}, JWT_SECRET, { expiresIn: "6h" })}
+     );
    } else{
      res.sendStatus(401)
    }
  }
    user!.validatePassword(password, handler)
+});
+
+app.get("/verify", expressjwt({secret: JWT_SECRET, algorithms: ["HS256"] }), (req: JWT_SECRET, res)=>{
+    console.log(req.auth);
+    if (req.auth?.user) res.sendStatus(200);
+    else res.sendStatus(401);
 });
 
 async function main() {
